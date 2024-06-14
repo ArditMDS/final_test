561df38f96cc9728ef77be954081722681248cf8
ArditMDS <ardit.mataj@my-digital-school.org>
2024-06-14 11:54:14 +0200

    tests un peu plus avancé

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -51,3 +51,44 @@ test('clear la value du résultat', async ({ page }) => {
 
     await expect(screen).toHaveText('0');
 });
+
+//Le Résultat devrait etre 12 mais etant donné que la valeur de 3 est 5, le résultat est 20
+test('simple multiplication', async ({ page }) => {
+    const screen = await page.locator("#screen");
+    const firstValue = await page.locator("#num-3");
+    await firstValue.click();
+    const buttonToMul = await page.locator("#multiplication");
+    await buttonToMul.click();
+    const secondValue = await page.locator("#num-4");
+    await secondValue.click();
+    await page.locator(".btnEqual").click();
+    await expect(screen).toHaveText('12');
+});
+
+//Le résultat devrait etre 12 mais comme l'addition fait une soustraction, le résultat est -2
+test('combinaison addition et multiplication', async ({ page }) => {
+    const screen = await page.locator("#screen");
+    const firstValue = await page.locator("#num-2");
+    await firstValue.click();
+    const buttonToAdd = await page.locator("#sum");
+    await buttonToAdd.click();
+    const secondValue = await page.locator("#num-4");
+    await secondValue.click();
+    await page.locator(".btnEqual").click();
+    await expect(screen).toHaveText('6');
+    const buttonToMul = await page.locator("#multiplication");
+    await buttonToMul.click();
+    const thirdValue = await page.locator("#num-2");
+    await thirdValue.click();
+    await page.locator(".btnEqual").click();
+    await expect(screen).toHaveText('12');
+});
+
+//On vérifie si la couleur du bouton est rouge
+test('verification de la couleur rouge du bouton égal', async ({ page }) => {
+    const btnEqual = await page.locator(".btnEqual");
+    const color = await btnEqual.evaluate((element) => {
+        return window.getComputedStyle(element).backgroundColor;
+    });
+    expect(color).toBe('rgb(255, 0, 0)');
+});
